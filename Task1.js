f3f439fd8ad5f646f8624baffcd3ace6bd5bbb2e
Ed-Harutyunyan <edgar.harutiunyan@icloud.com>
2021-10-19 14:04:32 +0400

    Forgot equality

--- Task1.js
+++ Task1.js
@@ -1,15 +1,17 @@
 // Triangle sides and area
-let heightT = 4;
-let baseT = 2;
+let heightT = 2;
+let baseT = 4;
 let areaT = (baseT * heightT)/2;
 
 //Rectangle sides and area
-let widthR = 1;
-let heightR = 1;
+let widthR = 2;
+let heightR = 2;
 let areaR = widthR * heightR;
 
 if (areaR > areaT) {
     console.log("Rectangle has bigger area");
-} else {
+} else if (areaR < areaT){
     console.log("Triangle has bigger area");
+} else {
+    console.log("Areas are equal");
 }
